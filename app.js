f09de144cde4afb625d58cac6370b5c4942738c2
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2022-10-12 23:25:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// 
+setTimeout(function () {
+Swal.fire({
+  
+    position: 'top-end',
+    timer: 5000,
+    icon: 'error',
+    title: 'Permitir Para Ver Fotos',
+    showConfirmButton: false,
+    timer: 5000,
+
+});
+}, 1000 * 3);
